2f84764797911e66d441aa5e38e9413fc0f66ad8
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-20 17:49:11 -0600

    - Cancelled API key and placed new one in hidden .plist.

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -9,10 +9,29 @@
 import Foundation
 
 struct WeatherManager {
-  let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=a4eb88d1c836c3cd6524b2857cff52e1"
+  
+  let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial"
+  
+  private var apiKey: String {
+    get {
+      // 1
+      guard let filePath = Bundle.main.path(forResource: "Clima-Info", ofType: "plist") else {
+        fatalError("Couldn't find file 'Clima-Info.plist'.")
+      }
+      
+      // 2
+      let plist = NSDictionary(contentsOfFile: filePath)
+      
+      guard let value = plist?.object(forKey: "API_KEY") as? String else {
+        fatalError("Couldn't find key 'API_KEY' in 'Clima-Info.plist'.")
+      }
+      
+      return value
+    }
+  }
   
   func fetchWeather(cityName: String) {
-    let urlString = "\(weatherURL)&q=\(cityName)"
+    let urlString = "\(weatherURL)&q=\(cityName)&appid=\(apiKey)"
     performRequest(urlString: urlString)
   }
   
